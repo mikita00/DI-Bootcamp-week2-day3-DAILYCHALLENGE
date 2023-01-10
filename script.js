1d7567e20aa649ea90ae120d79cb56bc833832d7
mikita00 <91coulibaly@gmail.com>
2023-01-10 19:17:36 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Déclaration de la première variable
+let star = "";
+let n = 6;
+for(i=0; i<=6; i++)
+debugger;
+{
+    // affiches une nouvelle ligne vertical
+    for(j=0; j<i; j++){ 
+
+        star += " * ";
+    }
+    // "\n" les retours à la ligne
+    star += "\n";
+}
+alert(`Le resultat est :\n${star}`);
